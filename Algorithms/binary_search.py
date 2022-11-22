12040210d204e5d40160917b964151cdc7cd235d
suyogrepale <suyogbrepale@gmail.com>
2022-11-22 17:00:20 +0530

    binary search info

---
+++ Algorithms/binary_search.py
@@ -0,0 +1,10 @@
+"""
+Linear search has Time Complexity of Big O(n)
+
+Binary Search works on Sorted List
+Start from Middle of the Sorted list
+Compare the element required with Middle element
+Divide depending on value greater or less and repeat
+
+Binary search has Time Complexity of Big Log(n)
+"""
